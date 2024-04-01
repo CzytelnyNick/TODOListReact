a8d6d434ab8ca05578a63dc15bd360ccdda92239
CzytelnyNick <odrowazd@gmail.com>
2024-04-01 15:08:23 +0200

    front

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-icons/icons/calendar2-check.svg'
-import Todoprompt from './components/Todoprompt';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/icons/calendar-check-fill.svg";
+
+import Todoprompt from "./components/Todoprompt";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <p className='text-center my-5 fs-1'>TODO Lista</p><i class="bi bi-calendar2-check"></i>
-        {
-          <Todoprompt></Todoprompt>
-        }
+        <p className="text-center my-5 fs-1">TODO Lista</p>
+        
+        <Todoprompt />
       </div>
     </div>
   );
--- src/components/Todoprompt.jsx
+++ src/components/Todoprompt.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-
-
+import React, {useState} from "react";
+import { ReactComponent as CalendarCheckFill } from "bootstrap-icons/icons/calendar-check-fill.svg";
 function Todoprompt() {
-    return (
-        
-            <div className='todoBar'><input class="form-control form-control-lg" type="text" aria-label=".form-control-lg example"/><button id="add" className='btn btn-success'>zatwierdź</button></div>
-        
-    )
+    const [arr, changeArr] = useState([])
+    const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+    function addTask(){
+
+    } 
+  return (
+    <div className="todo">
+      <input
+        class="form-control form-control-lg"
+        type="text"
+        id="input"
+        aria-label=".form-control-lg example"
+      />
+      <button onClick={addTask} id="add" className="btn btn-success">
+        <CalendarCheckFill />
+      </button>
+      <div className="todo__tasks"></div>
+    </div>
+  );
 }
 
-export default Todoprompt
+export default Todoprompt;
